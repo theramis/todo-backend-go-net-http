feab529d69fc386158d52cea23eaf2460b70607d
Simar Kalra <theramis@users.noreply.github.com>
2020-02-08 22:45:42 +1300

    Add cors try 2

--- src/todo-backend/main.go
+++ src/todo-backend/main.go
@@ -9,11 +9,18 @@ import (
 
 func handler(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Set("access-control-allow-origin", "*")
+	writer.Header().Set("access-control-allow-methods", "GET, POST, PATCH, DELETE")
+	writer.Header().Set("access-control-allow-headers", "accept, content-type")
 	_, _ = fmt.Fprint(writer, "Hello World!")
 }
 
 func main() {
 	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("Env var 'PORT' must be set")
+	}
+
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":" + port, nil))
 }

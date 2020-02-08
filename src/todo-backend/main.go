1b09b990497663a4d2fa1472384cefce6a88add7
Simar Kalra <theramis@users.noreply.github.com>
2020-02-08 23:04:56 +1300

    Quick hacky create todo method

--- src/todo-backend/main.go
+++ src/todo-backend/main.go
@@ -5,13 +5,27 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 func handler(writer http.ResponseWriter, request *http.Request) {
-	writer.Header().Set("access-control-allow-origin", "*")
-	writer.Header().Set("access-control-allow-methods", "GET, POST, PATCH, DELETE")
-	writer.Header().Set("access-control-allow-headers", "accept, content-type")
-	_, _ = fmt.Fprint(writer, "Hello World!")
+	addCorsHeaders(&writer)
+
+	switch request.Method {
+	case "POST":
+		createTodo(&writer, request)
+	default:
+		_, _ = fmt.Fprint(writer, "Hello World!")
+	}
+}
+
+func createTodo(writer *http.ResponseWriter, request *http.Request) {
+	w := *writer
+	w.Header().Set("Content-Type", "application/json")
+	contentLength, _ := strconv.Atoi(request.Header.Get("Content-Length"))
+	bodyBytes := make([]byte, contentLength)
+	request.Body.Read(bodyBytes)
+	w.Write(bodyBytes)
 }
 
 func main() {
@@ -21,6 +35,19 @@ func main() {
 		log.Fatal("Env var 'PORT' must be set")
 	}
 
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/todo", handler)
+	http.HandleFunc("/", catchAllHandler)
 	log.Fatal(http.ListenAndServe(":" + port, nil))
+}
+
+func catchAllHandler(writer http.ResponseWriter, request *http.Request) {
+	addCorsHeaders(&writer)
+	_, _ = fmt.Fprint(writer, "Hello World!")
+}
+
+func addCorsHeaders(writer *http.ResponseWriter) {
+	w := *writer
+	w.Header().Set("access-control-allow-origin", "*")
+	w.Header().Set("access-control-allow-methods", "GET, POST, PATCH, DELETE")
+	w.Header().Set("access-control-allow-headers", "accept, content-type")
 }
